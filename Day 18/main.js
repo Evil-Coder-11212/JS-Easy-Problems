7b2986f62f3190c3806d2d69977ac23b1e273c10
Guatam Sharma <gautamsharma13062009@gmail.com>
2022-03-10 12:02:43 +0530

    In Day 16 its week 5 an error was there in my PC

--- Day 18/main.js
+++ Day 18/main.js
@@ -1,5 +1,8 @@
 // Random Number Function and button click
 const guessBtn = document.getElementById('guess-btn');
+const hintEl = document.getElementById('hint-text');
+let hintMessage = "";
+const answerBtn = document.getElementById('answer-btn');
 const randomNumber = () =>{
     let total = Math.floor(Math.random()*100)+1;
     guessBtn.addEventListener('click', ()=>{
@@ -9,13 +12,115 @@ const randomNumber = () =>{
             timeEl.textContent = "Correct"
             countDown = 10;
             total = Math.floor(Math.random()*100)+1;
+            hintEl.innerText = `HINT: ${hintMessage}`
+            if(total <= 10){
+                hintMessage = "Less than or equal to 10 and greater than 0"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 20){
+                hintMessage = "Less than or equal to 20 and greater than 10"
+            }else if(total <= 30){
+                hintMessage = "Less than or equal to 30 and greater than 20"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 40){
+                hintMessage = "Less than or equal to 40 and greater than 30"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 50){
+                hintMessage = "Less than or equal to 50 and greater than 40"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 60){
+                hintMessage = "Less than or equal to 60 and greater than 50"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 70){
+                hintMessage = "Less than or equal to 70 and greater than 60"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 80){
+                hintMessage = "Less than or equal to 80 and greater than 70"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 90){
+                hintMessage = "Less than or equal to 90 and greater than 80"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 100){
+                hintMessage = "Less than or equal to 100 and greater than 90"
+            }else{
+                hintMessage = "An error occurs try referring the page"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }
         }else{
             timeEl.innerText = countDown;
             timeEl.textContent = "InCorrect"
             countDown = 10;
             total = Math.floor(Math.random()*100)+1;
+            hintEl.innerText = `HINT: ${hintMessage}`
+            if(total <= 10){
+                hintMessage = "Less than or equal to 10 and greater than 0"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 20){
+                hintMessage = "Less than or equal to 20 and greater than 10"
+            }else if(total <= 30){
+                hintMessage = "Less than or equal to 30 and greater than 20"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 40){
+                hintMessage = "Less than or equal to 40 and greater than 30"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 50){
+                hintMessage = "Less than or equal to 50 and greater than 40"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 60){
+                hintMessage = "Less than or equal to 60 and greater than 50"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 70){
+                hintMessage = "Less than or equal to 70 and greater than 60"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 80){
+                hintMessage = "Less than or equal to 80 and greater than 70"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 90){
+                hintMessage = "Less than or equal to 90 and greater than 80"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 100){
+                hintMessage = "Less than or equal to 100 and greater than 90"
+            }else{
+                hintMessage = "An error occurs try referring the page"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }
         }
     });
+    answerBtn.addEventListener('mousedown', ()=>{
+            timeEl.innerText = total;
+    });
+    hintEl.innerText = `HINT: ${hintMessage}`
+    if(total <= 10){
+        hintMessage = "Less than or equal to 10 and greater than 0"
+    hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 20){
+            hintMessage = "Less than or equal to 20 and greater than 10"
+    }else if(total <= 30){
+            hintMessage = "Less than or equal to 30 and greater than 20"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 40){
+            hintMessage = "Less than or equal to 40 and greater than 30"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 50){
+            hintMessage = "Less than or equal to 50 and greater than 40"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 60){
+            hintMessage = "Less than or equal to 60 and greater than 50"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 70){
+            hintMessage = "Less than or equal to 70 and greater than 60"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 80){
+            hintMessage = "Less than or equal to 80 and greater than 70"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 90){
+            hintMessage = "Less than or equal to 90 and greater than 80"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }else if(total <= 100){
+            hintMessage = "Less than or equal to 100 and greater than 90"
+    }else{
+            hintMessage = "An error occurs try referring the page"
+        hintEl.innerText = `HINT: ${hintMessage}`
+    }
     return total;
 }
 
@@ -32,11 +137,42 @@ const timer = () =>{
         countDown--
     }
     if(countDown === 0){
-        countDown = 10;
         randomNumber();
+        countDown = 10;
+        hintEl.innerText = `HINT: ${hintMessage}`
+        if(total <= 10){
+                hintMessage = "Less than or equal to 10 and greater than 0"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 20){
+                hintMessage = "Less than or equal to 20 and greater than 10"
+        }else if(total <= 30){
+                hintMessage = "Less than or equal to 30 and greater than 20"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 40){
+                hintMessage = "Less than or equal to 40 and greater than 30"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 50){
+                hintMessage = "Less than or equal to 50 and greater than 40"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 60){
+                hintMessage = "Less than or equal to 60 and greater than 50"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 70){
+                hintMessage = "Less than or equal to 70 and greater than 60"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 80){
+                hintMessage = "Less than or equal to 80 and greater than 70"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 90){
+                hintMessage = "Less than or equal to 90 and greater than 80"
+            hintEl.innerText = `HINT: ${hintMessage}`
+        }else if(total <= 100){
+                hintMessage = "Less than or equal to 100 and greater than 90"
+        }else{
+                hintMessage = "An error occurs try referring the page"
+        }
     }
 }, 1000)
 }
-
 // Calling Functions
 timer();
